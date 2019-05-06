b5f6f129d7809e12aa6e5d12f80dab4246073bf8
dinusha92 <dinushad92@gmail.com>
2019-05-06 10:53:42 +0530

    Adding properties to CRD file.

--- apim-operator/pkg/apis/wso2/v1alpha1/zz_generated.openapi.go
+++ apim-operator/pkg/apis/wso2/v1alpha1/zz_generated.openapi.go
@@ -67,10 +67,167 @@ func schema_pkg_apis_wso2_v1alpha1_APISpec(ref common.ReferenceCallback) common.
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
 				Description: "APISpec defines the desired state of API",
-				Properties:  map[string]spec.Schema{},
+				Properties: map[string]spec.Schema{
+					"name": {
+						SchemaProps: spec.SchemaProps{
+							Description: "INSERT ADDITIONAL SPEC FIELDS - desired state of cluster Important: Run \"operator-sdk generate k8s\" to regenerate code after modifying this file Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"context": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"version": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"description": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"tags": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+					"endpoints": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.Endpoint"),
+									},
+								},
+							},
+						},
+					},
+					"requestInterceptor": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"responseInterceptor": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"authorizationHeader": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"labels": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+					"urlPatterns": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.URLPattern"),
+									},
+								},
+							},
+						},
+					},
+					"security": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+					"subscriptionTiers": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+					"advancedThrottlingPolicy": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"businessInformation": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.BusinessInformation"),
+						},
+					},
+					"apiProperties": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.APIProperty"),
+									},
+								},
+							},
+						},
+					},
+					"mode": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
+					"replicaCount": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"integer"},
+							Format: "int32",
+						},
+					},
+				},
+				Required: []string{"name", "context", "version", "description", "tags", "endpoints", "requestInterceptor", "responseInterceptor", "authorizationHeader", "labels", "urlPatterns", "security", "subscriptionTiers", "advancedThrottlingPolicy", "businessInformation", "apiProperties", "mode", "replicaCount"},
 			},
 		},
-		Dependencies: []string{},
+		Dependencies: []string{
+			"github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.APIProperty", "github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.BusinessInformation", "github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.Endpoint", "github.com/apim-crd/apim-operator/pkg/apis/wso2/v1alpha1.URLPattern"},
 	}
 }
 
