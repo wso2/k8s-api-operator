e7f760b71cc9751fa9bde484966e20353ca38f6a
RameshaKaru <ramesha@wso2.com>
2019-05-07 16:56:59 +0530

    namespace changes

--- apim-operator/cmd/manager/main.go
+++ apim-operator/cmd/manager/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/apim-crd/apim-operator/pkg/apis"
 	"github.com/apim-crd/apim-operator/pkg/controller"
 
-	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
+	//"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
@@ -62,11 +62,11 @@ func main() {
 
 	printVersion()
 
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		log.Error(err, "Failed to get watch namespace")
-		os.Exit(1)
-	}
+	// namespace, err := k8sutil.GetWatchNamespace()
+	// if err != nil {
+	// 	log.Error(err, "Failed to get watch namespace")
+	// 	os.Exit(1)
+	// }
 
 	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
@@ -86,7 +86,7 @@ func main() {
 
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
-		Namespace:          namespace,
+		Namespace:          "",
 		MapperProvider:     restmapper.NewDynamicRESTMapper,
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	})
