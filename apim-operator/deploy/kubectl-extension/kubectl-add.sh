49ed89bc302001066208abdd2e7dd8ca06f53cf1
Shehanir <shehanir@wso2.com>
2019-06-11 14:28:22 +0530

    adding license header and requested changes

--- apim-operator/deploy/kubectl-extension/kubectl-add.sh
+++ apim-operator/deploy/kubectl-extension/kubectl-add.sh
@@ -1,3 +1,19 @@
+#Copyright (c)  WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+#
+# WSO2 Inc. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 #!/bin/bash
 # Please copy this file to /usr/local/bin and give executable permissions
 # Sample usage : kubectl add api api-name --from-file=/home/harsha/Downloads/boomi/non-weborders/order_swagger.json --replicas=3
