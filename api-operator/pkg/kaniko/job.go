7854d3048663b1b18629c34ee71a37c764e7ca31
Renuka Fernando <renuka@wso2.com>
2020-04-23 09:08:38 +0530

    Add MGW service, HPA, ingress, route to mgw package wso2/k8s-api-operator#326

--- api-operator/pkg/kaniko/job.go
+++ api-operator/pkg/kaniko/job.go
@@ -1,19 +1,26 @@
 package kaniko
 
 import (
+	"github.com/golang/glog"
 	wso2v1alpha1 "github.com/wso2/k8s-api-operator/api-operator/pkg/apis/wso2/v1alpha1"
 	"github.com/wso2/k8s-api-operator/api-operator/pkg/registry"
 	"github.com/wso2/k8s-api-operator/api-operator/pkg/volume"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"strings"
 )
+
+var logJob = log.Log.WithName("kaniko.job")
 
 const (
 	kanikoImgConst = "kanikoImg"
 )
 
+// Job returns a kaniko job with mounted volumes
 func Job(api *wso2v1alpha1.API, controlConfigData map[string]string, kanikoArgs string, owner *[]metav1.OwnerReference) *batchv1.Job {
 	rootUserVal := int64(0)
 	jobName := api.Name + "-kaniko"
@@ -71,4 +78,44 @@ func Job(api *wso2v1alpha1.API, controlConfigData map[string]string, kanikoArgs 
 			},
 		},
 	}
+}
+
+// DeleteCompletedJob deletes completed kaniko jobs
+func DeleteCompletedJob(namespace string) error {
+	logJob.Info("Deleting completed kaniko job")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		glog.Errorf("Can't load in cluster config: %v", err)
+		return err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Errorf("Can't get client set: %v", err)
+		return err
+	}
+
+	deletePolicy := metav1.DeletePropagationBackground
+	deleteOptions := metav1.DeleteOptions{PropagationPolicy: &deletePolicy}
+	//get list of exsisting jobs
+	getListOfJobs, errGetJobs := clientset.BatchV1().Jobs(namespace).List(metav1.ListOptions{})
+	if len(getListOfJobs.Items) != 0 {
+		for _, kanikoJob := range getListOfJobs.Items {
+			if kanikoJob.Status.Succeeded > 0 {
+				logJob.Info("Job "+kanikoJob.Name+" completed successfully", "Job.Namespace", kanikoJob.Namespace, "Job.Name", kanikoJob.Name)
+				logJob.Info("Deleting job "+kanikoJob.Name, "Job.Namespace", kanikoJob.Namespace, "Job.Name", kanikoJob.Name)
+				//deleting completed jobs
+				errDelete := clientset.BatchV1().Jobs(kanikoJob.Namespace).Delete(kanikoJob.Name, &deleteOptions)
+				if errDelete != nil {
+					logJob.Error(errDelete, "error while deleting "+kanikoJob.Name+" job")
+					return errDelete
+				} else {
+					logJob.Info("successfully deleted job "+kanikoJob.Name, "Job.Namespace", kanikoJob.Namespace, "Job.Name", kanikoJob.Name)
+				}
+			}
+		}
+	} else if errGetJobs != nil {
+		logJob.Error(errGetJobs, "error retrieving jobs")
+		return err
+	}
+	return nil
 }
