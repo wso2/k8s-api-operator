2911fb89939d660ba97383a7a900a55d4393b73c
Renuka Fernando <renukapiyumal@gmail.com>
2020-10-14 15:39:50 +0530

    Add ingress controller

---
+++ api-operator/pkg/controller/ingress/ingress_controller.go
@@ -0,0 +1,95 @@
+package ingress
+
+import (
+	"context"
+	"github.com/wso2/k8s-api-operator/api-operator/pkg/ingress/class"
+	"k8s.io/api/networking/v1beta1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_ingress")
+
+// Add creates a new Ingress Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileIngress{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("ingress-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource Ingress
+	err = c.Watch(&source.Kind{Type: &v1beta1.Ingress{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileIngress implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileIngress{}
+
+// ReconcileIngress reconciles a Ingress object
+type ReconcileIngress struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Ingress object and makes changes based on the state read
+// and what is in the Ingress.Spec
+func (r *ReconcileIngress) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling Ingress")
+
+	// Fetch the Ingress instance
+	instance := &v1beta1.Ingress{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	if !class.IsValid(instance) {
+		reqLogger.Info("Ignore ingress based on ingress class")
+		return reconcile.Result{}, nil
+	}
+
+	// Set Ingress instance as the owner and controller
+	//if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
+	//	return reconcile.Result{}, err
+	//}
+
+	// Check if this Pod already exists
+
+	// Pod already exists - don't requeue
+	//reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
+	return reconcile.Result{}, nil
+}
---
+++ api-operator/pkg/ingress/class/class.go
@@ -0,0 +1,50 @@
+package class
+
+import (
+	"k8s.io/api/networking/v1beta1"
+)
+
+const (
+	// DefaultName defines the default class used in the WSO2 API Microgateway ingress controller
+	DefaultName = "microgateway"
+)
+
+var (
+	// IngressClass sets the runtime ingress class to use
+	// An empty string means accept all ingresses without
+	// annotation and the ones configured with class microgateway
+	IngressClass = "microgateway"
+)
+
+// IsValid returns true if the given Ingress specify the ingress.class annotation
+// or IngressClassName resource for Kubernetes >= v1.18
+func IsValid(ing *v1beta1.Ingress) bool {
+	// with annotation
+	ingress, ok := ing.Annotations[v1beta1.AnnotationIngressClass]
+	if ok {
+		//// empty annotation and same annotation on ingress
+		//if ingress == "" && IngressClass == DefaultName {
+		//	return true
+		//}
+		//
+		//return ingress == IngressClass
+
+		return ingress == DefaultName
+	}
+
+	//// 2. k8s < v1.18. Check default annotation
+	//if !k8s.IsIngressV1Ready {
+	//	return IngressClass == DefaultClass
+	//}
+	//
+	//// 3. without annotation and IngressClass. Check default annotation
+	//if k8s.IngressClass == nil {
+	//	return IngressClass == DefaultClass
+	//}
+	//
+	//// 4. with IngressClass
+	//if ing.Spec.IngressClassName != nil {
+	//	return k8s.IngressClass.Name == *ing.Spec.IngressClassName
+	//}
+	return false
+}
